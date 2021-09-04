56d6e2588e4253e476ce6a5fa37adeb4bd847339
Nan-u <analogy1431@gmail.com>
2021-09-04 02:56:27 -0700

    firs commit

---
+++ src/components/Page1.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  font-family: 'Didact Gothic', sans-serif;
+  font-size: 18px;
+  flex-direction: column;
+  padding-left: 30px;
+  padding-right: 30px;
+  width: 80%;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+const Title = styled.div`
+  font-size: 30px;
+  font-weight: bold;
+  cursor: pointer;
+  margin-top: 10px;
+  margin-bottom: 30px;
+`;
+
+const Desc = styled.div`
+  text-align: left;
+  margin-bottom: 40px;
+`;
+
+class Page1 extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      show: true,
+    }
+
+    this.showClick = this.showClick.bind(this);
+  }
+
+  showClick() {
+    const { show } = this.state;
+    this.setState({
+      show: !show
+    });
+  }
+
+  render() {
+
+    const { show } = this.state;
+    return (
+      <Wrapper>
+        <Title onClick={() => this.showClick()}>WTF?</Title>
+        {
+          show
+          ? <Desc>Nouns are an experimental attempt to improve the formation of on-chain avatar communities. While projects such as Cryptopunks have attempted to bootstrap digital community and identity, Nouns attempt to bootstrap identity, community, governance and a treasury that can be used by the community for the creation of long-term value.
+          Learn more about on-chain nouns below, or make some off-chain nouns using nouns playground.</Desc>
+          : <Desc></Desc>
+        }
+      </Wrapper>
+    )
+  }
+}
+
+export default Page1;
---
+++ src/components/WebPage.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import famous from './famous.svg';
+import styled from 'styled-components';
+
+import Page from './Page.jsx';
+import Page0 from './Page0.jsx';
+
+const ImgWrapper = styled.img`
+  height: 40px;
+  width: 40px;
+  margin-right: 10px;
+`;
+
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 40px;
+`;
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  font-family: 'Pacifico', cursive;
+  font-size: 30px;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 50px;
+`;
+
+class WebPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <PageWrapper>
+        <HeaderWrapper><ImgWrapper src={famous} />The List by King Alexander</HeaderWrapper>
+        <Page />
+        <Page0 />
+      </PageWrapper>
+    )
+  }
+}
+
+export default WebPage;
